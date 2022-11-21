36c11b5d090ff2430816edf00679c473fc13f4bc
Dylan Wilson (bamhm182) <bamhm182@gmail.com>
2022-11-21 13:45:10 +0000

    Added more to the sanitizer

--- src/synack/plugins/alerts.py
+++ src/synack/plugins/alerts.py
@@ -6,6 +6,7 @@ Functions to handle sending alerts to various clients
 import email
 import datetime
 import json
+import re
 import requests
 import smtplib
 
@@ -35,6 +36,33 @@ class Alerts(Plugin):
 
         server.login(self.db.smtp_username, self.db.smtp_password)
         server.send_message(msg)
+
+    def sanitize(self, message):
+        message = re.sub(r'[-a-zA-Z0-9@:%._+~#=\]{1,256}\.[a-zA-Z0-9()]{1,6}\b' +
+                         r'([-a-zA-Z0-9()@:%_\+.~#?&//=]*)', '[REDACTED]', message)
+        message = re.sub(r'(?:https?:\\/\\/)?(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}' +
+                         r'\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)', '[REDACTED]', message)
+        message = re.sub(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.[0-9]{1,3}', '[REDACTED]', message)
+        ipv6_regex = r''.join([
+            r'(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|',
+            r'([0-9a-fA-F]{1,4}:){1,7}:|',
+            r'([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|',
+            r'([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|',
+            r'([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|',
+            r'([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|',
+            r'([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|',
+            r'[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|',
+            r':((:[0-9a-fA-F]{1,4}){1,7}|:)|',
+            r'fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|',
+            r'::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|',
+            r'(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|',
+            r'(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|',
+            r'([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|',
+            r'(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|',
+            r'(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))',
+        ])
+        message = re.sub(ipv6_regex, '[REDACTED]', message)
+        return message
 
     def slack(self, message='This is a test'):
         requests.post(self.db.slack_url,
