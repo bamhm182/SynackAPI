f660f6fa85a4e7476ebbc29b1357db7522a4cb9d
Dylan Wilson (bamhm182) <bamhm182@gmail.com>
2022-11-29 00:57:30 +0000

    Updated dependencies

--- setup.py
+++ setup.py
@@ -26,14 +26,14 @@ setuptools.setup(
     },
     package_dir={'': 'src'},
     install_requires=[
+        "alembic==1.8.1",
         "netaddr==0.8.0",
-        "pathlib2==2.3.5",
-        "psycopg2-binary==2.9.1",
-        "pyaml==21.8.3",
-        "pyotp==2.6.0",
-        "requests==2.25.1",
-        "SQLAlchemy==1.4.31",
-        "alembic==1.7.5",
-        "urllib3==1.26.3",
+        "pathlib2==2.3.6",
+        "psycopg2-binary==2.9.5",
+        "pyaml==21.10.1",
+        "pyotp==2.7.0",
+        "requests==2.28.1",
+        "SQLAlchemy==1.4.44",
+        "urllib3==1.26.13",
     ]
 )
